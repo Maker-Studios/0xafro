f2c40b64e5084dc51ed8e15212e13b784c6117c8
Maker Studios <mashamipaccy04@gmail.com>
2024-01-31 17:20:38 +0200

    feat:made changes on the product card

--- packages/nextjs/components/AccountDetailsComponents/ProjectCard.tsx
+++ packages/nextjs/components/AccountDetailsComponents/ProjectCard.tsx
@@ -1,13 +1,45 @@
+import { CompletedSvg, LiveSvg, OngoingSvg } from "../Icons/Icons";
+import { cn } from "~~/lib/utils";
+
+/* eslint-disable @next/next/no-img-element */
 interface ProductCardProps {
-  icon: React.ReactNode;
-  settingTitle: string;
-  color: string;
-  bgColor: string;
   image: string;
   description: string;
-  projectName: string;
+  name: string;
+  status: "ongoing" | "completed" | "live";
 }
-const ProductCard = ({ icon, settingTitle, color, bgColor, image, description, projectName }: ProductCardProps) => {
+
+const ProductCard = ({ image, description, name, status }: ProductCardProps) => {
+  const handleStatusAccent = () => {
+    if (status === "ongoing") {
+      return {
+        color: "#9747FF",
+        bgColor: "#F4F3FF",
+      };
+    }
+
+    if (status === "completed") {
+      return {
+        color: "#2360FF",
+        bgColor: "#F4F3FF",
+      };
+    }
+
+    if (status === "live") {
+      return {
+        color: "#03A300",
+        bgColor: "#F6FBF6",
+      };
+    }
+
+    return {
+      color: "",
+      bgColor: "",
+    };
+  };
+
+  const accent: { color: string; bgColor: string } = handleStatusAccent();
+
   return (
     <div
       className="flex space-x-4 border-[0.5px] border-[#CDDFE9] rounded-2xl p-4 bg-white"
@@ -20,16 +52,20 @@ const ProductCard = ({ icon, settingTitle, color, bgColor, image, description, p
       </div>
       <div className="flex flex-col justify-between space-y-6">
         <div className="space-y-2">
-          <p className="font-medium leading-6">{projectName}</p>
+          <p className="font-medium leading-6">{name}</p>
           <p className="text-[12px] text-[#878787] font-medium leading-[18px]">{description}</p>
         </div>
         <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-1 py-1 px-2 rounded-full" style={{ background: bgColor }}>
-            {icon}
-            <p className="text-[12px]" style={{ color: color }}>
-              {settingTitle}
+          <div className="flex items-center space-x-1 py-1 px-2 rounded-full" style={{ background: accent.bgColor }}>
+            {status === "ongoing" && <OngoingSvg />}
+            {status === "completed" && <CompletedSvg />}
+            {status === "live" && <LiveSvg />}
+
+            <p className={cn("text-[12px]")} style={{ color: accent.color }}>
+              {status}
             </p>
           </div>
+
           <div className="flex items-center space-x-2">
             <p className="text-[12px] font-medium text-[#9F9F9F]">Spend</p>
             <span className="flex items-center space-x-1">
